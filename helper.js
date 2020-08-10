482351b91f59ea6f231a681c0cc4ad56380536ff
Batchyomna <60706911+Batchyomna@users.noreply.github.com>
2020-08-10 02:15:12 +0200

    Update helper.js
    update the function newGroup to be able to dislpay all available possibilities from student groups

--- helper.js
+++ helper.js
@@ -43,17 +43,25 @@ async function newGroup(project, num) {
         let myData = await myMongo.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
         var myDataBase = myData.db("FullStack");
         var students = await myDataBase.collection("Students").find().toArray()
-         let names = [];
-         //for(let j=0; j<students.length; j++){
-        for(let i=0; i<num; i++){
-            let i = Math.floor(Math.random()*students.length);
-            var selectedStudent = students.splice(i, 1)// with splice we are sur that we will not take the same student twice
-            names.push(selectedStudent[0].name)  
+        let names = [];
+        let groups = [];
+        if (num != 0) {
+            while(students.length >= num) {// it will be reapeted as much as the number of the groups possible
+                for (let i = 0; i < num; i++) {//it will reapeted as much as the number of the students required in every group 
+                    let i = Math.floor(Math.random() * students.length);
+                    var selectedStudent = students.splice(i, 1)// with splice we are sur that we will not take the same student twice
+                    names.push(selectedStudent[0].name)
+                }
+                let x = names.join(' + ')//to change all the elements to one string separates by(+)
+             groups.push(x)
+             names = [];
+            }        
+            await myDataBase.collection("Groups").insertOne({ name: project, num: num, studentsNames: groups })
+
+        }else{
+            console.log(" You have to enter a number of students other than zero ")
         }
-        let x = names.join(' + ')//to change all the elements to one string separates by(+)
-        await myDataBase.collection("Groups").insertOne({name:project, num : num, studentsNames: x})
-   
-    }catch (err) {
+    } catch (err) {
         myData.close()
 
     }
